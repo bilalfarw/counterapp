66e2236848fbec9409f353e8573d715772e590c5
Bilal Mohamed <bilalfarw@gmail.com>
2019-06-27 10:55:44 +0200

    Adding Counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     list: ["bilal", "omar", "mohamed"]
   };
 
@@ -16,7 +16,7 @@ class Counter extends Component {
         this.handelincerment refers to an object in js , then u need to */}
         <button
           onClick={() => {
-            this.handelIncerment({ id: this.state.count });
+            this.handelIncerment({ id: this.state.value });
           }}
           className="btn btn-secondary btn-sm m-2"
         >
@@ -30,32 +30,32 @@ class Counter extends Component {
           -
         </button>
 
+        {/*
         <div>
-          {/* if condition but dosent have an else */}
+           if condition but dosent have an else 
           {this.state.count === 0 && "Please Count = 0 "}
-          {/* to see if the list is empty or what */}
+           to see if the list is empty or what 
           {this.renderTags()}
-        </div>
+        </div>*/}
       </React.Fragment>
     );
   }
 
   Class() {
     let classes = "badge  m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   handelIncerment = id => {
-    this.setState({ count: this.state.count + 1 });
-    console.log(id);
+    this.setState({ value: this.state.value + 1 });
   };
   handelDecrement = () => {
-    if (this.state.count > 0) this.setState({ count: this.state.count - 1 });
+    if (this.state.value > 0) this.setState({ value: this.state.value - 1 });
   };
 
   plus() {
-    return this.state.count === 0 ? "ZERO" : this.state.count;
+    return this.state.value === 0 ? "ZERO" : this.state.value;
   }
   renderTags() {
     if (this.state.list.length === 0) return <p>The List is Empty!</p>;
